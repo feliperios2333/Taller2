1df7edbbd98a49ff4b44cd8865cdadfe9866d77a
feliperios2333 <feliperios2003brs@gmail.com>
2025-03-11 20:01:47 -0500

    datos php

---
+++ formulario.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['nombre'];
+$direccion = $_POST['direccion'];
+$cidudad = $_POST['ciudad'];
+$peso= $_POST['peso'];
+$servicio = $_POST['servicio'];
+$valor = $_POST['valor'];
+
+?>
